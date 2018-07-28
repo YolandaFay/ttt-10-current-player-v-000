ccf0b21e55dab872d686695ba0d82dab3ce4a823
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 18:49:24 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,11 @@
 #TURN_COUNT method code##############################
 
 def turn_count(board)
+  turn_count = 0 
   
+  board.each do |entry|
+    if entry != " "
+      turn_count =+ 1 
+  end
   
 end 
